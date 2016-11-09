7289d33dbed12ccfa400e95f785c1403d577b219
markel95 <markelarra95@gmail.com>
2016-11-09 09:11:59 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,96 @@
+<?php
+
+// Call set_include_path() as needed to point to your client library.
+require_once __DIR__ . '/vendor/autoload.php';
+session_start();
+
+/*
+ * You can acquire an OAuth 2.0 client ID and client secret from the
+ * Google Developers Console <https://console.developers.google.com/>
+ * For more information about using OAuth 2.0 to access Google APIs, please see:
+ * <https://developers.google.com/youtube/v3/guides/authentication>
+ * Please ensure that you have enabled the YouTube Data API for your project.
+ */
+
+$client = new Google_Client();
+$client->setClientId("613558108913-9hos7ugr12dnj3cjpegpgonm341c1dc6.apps.googleusercontent.com");
+$client->setClientSecret("1kus9SQ7nCHFjOxZncm2l6Lw");
+$client->setScopes('https://www.googleapis.com/auth/youtube');
+$redirect = "https://youtube-markel-markel95.c9users.io/index.php";
+$client->setRedirectUri($redirect);
+
+// Define an object that will be used to make all API requests.
+$youtube = new Google_Service_YouTube($client);
+
+if (isset($_GET['code'])) {
+  if (strval($_SESSION['state']) !== strval($_GET['state'])) {
+    die('The session state did not match.');
+  }
+
+  $client->authenticate($_GET['code']);
+  $_SESSION['token'] = $client->getAccessToken();
+  header('Location: ' . $redirect);
+}
+
+if (isset($_SESSION['token'])) {
+  $client->setAccessToken($_SESSION['token']);
+}
+
+// Check to ensure that the access token was successfully acquired.
+if ($client->getAccessToken()) {
+  try {
+    // Call the channels.list method to retrieve information about the
+    // currently authenticated user's channel.
+    $channelsResponse = $youtube->channels->listChannels('contentDetails', array(
+      'mine' => 'true',
+    ));
+
+    $htmlBody = '';
+    foreach ($channelsResponse['items'] as $channel) {
+      // Extract the unique playlist ID that identifies the list of videos
+      // uploaded to the channel, and then call the playlistItems.list method
+      // to retrieve that list.
+      $uploadsListId = $channel['contentDetails']['relatedPlaylists']['uploads'];
+
+      $playlistItemsResponse = $youtube->playlistItems->listPlaylistItems('snippet', array(
+        'playlistId' => $uploadsListId,
+        'maxResults' => 50
+      ));
+      $htmlBody .= "<h3>Videos en $uploadsListId</h3><ul>";
+      foreach ($playlistItemsResponse['items'] as $playlistItem) {
+        $htmlBody .= sprintf('<li>%s (%s)</li>', $playlistItem['snippet']['title'],
+          $playlistItem['snippet']['resourceId']['videoId']);
+      }
+      $htmlBody .= '</ul>';
+    }
+  } catch (Google_Service_Exception $e) {
+    $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  } catch (Google_Exception $e) {
+    $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  }
+
+  $_SESSION['token'] = $client->getAccessToken();
+} else {
+  $state = mt_rand();
+  $client->setState($state);
+  $_SESSION['state'] = $state;
+
+  $authUrl = $client->createAuthUrl();
+  $htmlBody = <<<END
+  <h3>Authorization Required</h3>
+  <p>You need to <a href="$authUrl">authorize access</a> before proceeding.<p>
+END;
+}
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>Mis videos subidos</title>
+  </head>
+  <body>
+    <?=$htmlBody?>
+  </body>
+</html>
